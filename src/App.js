7665c1c0d575d125bdde114fb03b179ed4324a28
CDLarsen <curtis.larssen@gmail.com>
2020-04-06 22:20:18 +1300

    CHANGE IMG SIZE

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import Nav from './components/Nav'
 import Special from './components/Special'
+import Services from './components/Services'
 
 function App() {
   return (
     <>
 
-      <Special />
+      <Services />
       {/* <Nav />
       <div className='container'>
       </div> */}
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,64 +1,64 @@
 import React from 'react'
-import xray from '../images/xray-icon'
-import blah from '../images/'
-import blah from '../images/'
-import blah from '../images/'
-import blah from '../images/'
-import blah from '../images/'
-import blah from '../images/'
-import blah from '../images/'
+import xrayIcon from '../images/xray-icon.png'
+import ultrasoundIcon from '../images/ultrasound-icon.png'
+import heartIcon from '../images/heart-icon.png'
+import checkupIcon from '../images/checkup-icon.png'
+import vaccinationIcon from '../images/vaccination-icon.png'
+import otoscopeIcon from '../images/otoscope-icon.png'
+import endoscopeIcon from '../images/endoscope-icon.png'
+import bloodIcon from '../images/blood-icon.png'
 
 
 
 export default function Special() {
     return (
         <div className='services-wrapper'>
-            <ul className='space-around grid'>
+            <ul className='space-around grid' id='service-grid'>
                 <li>
-                    <img
-                        src=""
+                    <img className='serv-img'
+                        src={xrayIcon}
                         alt="X-Ray 검사"
                     />
                 </li>
                 <li>
                     <img
-                        src=""
+                        src={ultrasoundIcon}
                         alt="초음파검사"
                     />
                 </li>
                 <li>
                     <img
-                        src=""
+                        src={heartIcon}
                         alt="심전도검사"
                     />
                 </li>
                 <li>
                     <img
-                        src=""
+                        src={checkupIcon}
                         alt="종합건강검진"
                     />
                 </li>
                 <li>
                     <img
-                        src=""
+                        src={vaccinationIcon}
                         alt="예방접종"
                     />
                 </li>
                 <li>
                     <img
-                        src=""
+                        src={otoscopeIcon}
                         alt="귀이경검사"
                     />
                 </li>
                 <li>
                     <img
-                        src=""
+                        src={endoscopeIcon}
                         alt="수면내시경"
                     />
                 </li>
                 <li>
                     <img
-                        src=""
+                        src={bloodIcon}
                         alt="혈액검사"
                     />
                 </li>
