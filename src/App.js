0225a8b912ba7644e8221c7fc5d73d3b1efe7b58
CDLarsen <curtis.larssen@gmail.com>
2020-04-10 17:34:01 +1300

    Gallery

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from './components/Nav'
 import Special from './components/Special'
 import Services from './components/Services'
 import Landing from './components/Landing'
+import Gallery from './components/Gallery'
 import { BrowserRouter as Router, Route, Switch, } from 'react-router-dom'
 
 
@@ -16,6 +17,7 @@ function App() {
         <Route path='/' exact component={Landing}/>
         <Route path='/services' component={Services}/>
         <Route path='/special' component={Special}/>
+        <Route path='/gallery' component={Gallery}/>
       </Switch>
 
     </Router>
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,3 +1,66 @@
 import React from 'react'
-import p1 from '../images/gallery/gallery1
-' 
+import p1 from '../images/gallery/gallery1'
+import p2 from '../images/gallery/gallery2'
+import p3 from '../images/gallery/gallery3'
+import p4 from '../images/gallery/gallery4'
+import p5 from '../images/gallery/gallery5'
+import p6 from '../images/gallery/gallery6'
+import p7 from '../images/gallery/gallery7'
+import p8 from '../images/gallery/gallery8'
+import p9 from '../images/gallery/gallery9'
+import p10 from '../images/gallery/gallery10'
+import p11 from '../images/gallery/gallery11'
+import p12 from '../images/gallery/gallery12'
+import p13 from '../images/gallery/gallery13'
+import p14 from '../images/gallery/gallery14'
+
+export default function Gallery() {
+    return (
+        <div className='gallery-wrapper'>
+            <ul className='gallery-grid'>
+                <li>
+                    <img src={p1} alt="p1"/>
+                </li>
+                <li>
+                    <img src={p2} alt="p2"/>
+                </li>
+                <li>
+                    <img src={p3} alt="p3"/>
+                </li>
+                <li>
+                    <img src={p4} alt="p4"/>
+                </li>
+                <li>
+                    <img src={p5} alt="p5"/>
+                </li>
+                <li>
+                    <img src={p6} alt="p6"/>
+                </li>
+                <li>
+                    <img src={p7} alt="p7"/>
+                </li>
+                <li>
+                    <img src={p8} alt="p8"/>
+                </li>
+                <li>
+                    <img src={p9} alt="p9"/>
+                </li>
+                <li>
+                    <img src={p10} alt="p10"/>
+                </li>
+                <li>
+                    <img src={p11} alt="p11"/>
+                </li>
+                <li>
+                    <img src={p12} alt="p12"/>
+                </li>
+                <li>
+                    <img src={p13} alt="p13"/>
+                </li>
+                <li>
+                    <img src={p14} alt="p14"/>
+                </li>
+            </ul>
+        </div>
+    )
+}
