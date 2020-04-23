f24429ee9138ceb1d50d3bd58002f2a65e969468
CDLarsen <curtis.larssen@gmail.com>
2020-04-23 22:30:41 +1200

    1

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'react-google-maps';
-
-function Map() {
-    return (
-        <GoogleMapReact
-            bootstrapURLKeys={{ key: 'AIzaSyAK_LgrzatV4KZsNOBQ2jXjUJxjlbFAMZs' }}
-            defaultCenter={{ lat: 59.95, lng: 30.33 }}
-            defaultZoom={11}
-        />
-
-    )
+import React from 'react';
+import {mapPic} from '../images/map.png'
+export default function Map() {
+  return (
+    <div className='map-wrapper'>
+      <div className='info-box'>
+        <div><h1 style={{textAlign:'center'}}>秀수내과</h1></div>
+        <div>22</div>
+        <div>33</div>
+        <div>44</div>
+      </div>
+      <div className='map-box'>
+        <img src={mapPic} alt='Clinic Map'/>
+      </div>
+    </div>
+  )
 }
-
-export default Map;
